39e28ea35127136fdb5bf54f94f0134fa81caed2
AlejandrodePablo <115726867+AlejandrodePablo@users.noreply.github.com>
2022-11-25 14:58:38 +0100

    Classes changes

--- src/Autonomous.java
+++ src/Autonomous.java
@@ -1,4 +1,4 @@
-public class Autonomous {
+public class Autonomous implements Client {
     private String name;
     private String surname;
     private String dni;
@@ -7,6 +7,70 @@ public class Autonomous {
     private Integer  postalCode;
     private String province;
     private String email;
-    private Integer Telephone;
+    private Integer telephone;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    public void setCodClient(Integer codClient) {
+        this.codClient = codClient;
+    }
+    @Override
+    public Integer getCodClient() {
+        return codClient;
+    }
+    public void setPoblation(String poblation) {
+        this.poblation = poblation;
+    }
+    @Override
+    public String getPoblation() {
+        return poblation;
+    }
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+    @Override
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    @Override
+    public String getProvince() {
+        return province;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Override
+    public String getEmail() {
+        return email;
+    }
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+    @Override
+    public Integer getTelephone() {
+        return telephone;
+    }
 }
--- src/Bussines.java
+++ src/Bussines.java
@@ -1,4 +1,4 @@
-public class Bussines {
+public class Bussines implements Client{
     private String name;
     private String cif;
     private Integer codClient;
@@ -7,4 +7,62 @@ public class Bussines {
     private String province;
     private String email;
     private Integer telephone;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+    public void setCodClient(Integer codClient) {
+        this.codClient = codClient;
+    }
+    @Override
+    public Integer getCodClient() {
+        return codClient;
+    }
+    public void setPoblation(String poblation) {
+        this.poblation = poblation;
+    }
+    @Override
+    public String getPoblation() {
+        return poblation;
+    }
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+    @Override
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    @Override
+    public String getProvince() {
+        return province;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+
+    }
+        @Override
+    public String getEmail() {
+        return email;
+    }
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+    @Override
+    public Integer getTelephone() {
+        return telephone;
+    }
 }
